a1a7fb032281afea9ef37c281abd98df118e95d6
HeliXxXxX <138361735+HeliXxXxX@users.noreply.github.com>
2023-11-20 12:57:17 +0100

    Delete 06.1.1 directory

--- 06.1.1/06.1.1.py
+++
@@ -1,5 +0,0 @@
-with open("C:\\Users\\hirun\\OneDrive\\Desktop\\Computer science\\Lab6\\06.1.1\\input.txt", "r") as f:
-    n = 0
-    for line in f:
-        n += 1
-        print(f"/*{n}*/ {line.strip()}")
