7da4319d6d85953509bf7e1d7232508dba444a05
HeliXxXxX <138361735+HeliXxXxX@users.noreply.github.com>
2023-11-20 16:08:34 +0100

    Update 06.1.1.py

--- Lab6/06.1.1/06.1.1.py
+++ Lab6/06.1.1/06.1.1.py
@@ -1,5 +1,6 @@
 with open("C:\\Users\\hirun\\OneDrive\\Desktop\\Computer science\\Lab6\\06.1.1\\input.txt", "r") as f:
-    n = 0
-    for line in f:
-        n += 1
-        print(f"/*{n}*/ {line.strip()}")
+    with open("C:\\Users\\hirun\\OneDrive\\Desktop\\Computer science\\Lab6\\06.1.1\\output.txt", "w") as f2:
+        n = 0
+        for line in f:
+            n += 1
+            f2.write(f"/*{n}*/ {line.strip()} \n")
