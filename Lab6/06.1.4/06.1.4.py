ce35d634ffbd9cf1b7dd3f002b6a54da0d310e43
HeliXxXxX <138361735+HeliXxXxX@users.noreply.github.com>
2023-11-23 12:09:36 +0100

    Update 06.1.4.py

--- Lab6/06.1.4/06.1.4.py
+++ Lab6/06.1.4/06.1.4.py
@@ -1,26 +1,11 @@
 try:
     with open("Lab6\\06.1.4\\input.txt", "r") as file:
         for line in file:
-            count = 0
-            service = []
-            price = []
-            for letter in line:
-                if letter == ";":
-                    count += 1
-                if count == 1:
-                    service.append(letter)
-
-                elif count == 2:
-                    price.append(letter)
-                else:
-                    None
-            service.pop(0)
-            price.pop(0)
-            print(f"service: {''.join(service)}  price:  {''.join(price)}")
-
-
+            list1 = []
+            A = line.split(';')
+            list1.append(A)
+            print(f"service: {list1[0][1]}  price:  {list1[0][2]}")
 except FileNotFoundError:
     print("Error: The file does not exist.")
-
 except IOError:
     print("Error: Could not read the file.")
